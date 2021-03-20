243fa7ed64ba746933f68aa7dde692d132352162
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-19 22:10:25 -0700

    activate the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,3 +16,22 @@ self.addEventListener('install', function (e) {
         })
     )
 })
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            })
+            );
+        })
+    );
+});
