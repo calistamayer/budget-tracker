d82267df57d14305bb1902828bb071315826e88b
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-19 21:59:11 -0700

    install service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,18 @@
 const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
-    "./js/idb.js",
-    "./js/index.js"
+    "./js/index.js",
+    "./js/idb.js"
 ];
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
-self.addEventListener('install', function(e){
-
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 })
