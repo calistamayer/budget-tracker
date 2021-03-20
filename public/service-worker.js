c4059478a8703e3003f3b3f5023b45af742665f6
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-19 22:24:57 -0700

    intercept fetch requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -35,3 +35,18 @@ self.addEventListener('activate', function (e) {
         })
     );
 });
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
